1ec87ffc0d7ac7348c9290c2b50c80507a3d4e3a
se030 <se030.kim@gmail.com>
2023-02-12 22:35:37 +0900

    config: 절대경로 설정
    Co-authored-by: hodun <dohun31@users.noreply.github.com>
    Co-authored-by: Hyunbin Lee <iyu88@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { resolve } from 'path';
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@src', replacement: resolve(__dirname, './src') },
+      {
+        find: '@components',
+        replacement: resolve(__dirname, './src/components'),
+      },
+      {
+        find: '@styles',
+        replacement: resolve(__dirname, './src/styles'),
+      },
+    ],
+  },
 });
