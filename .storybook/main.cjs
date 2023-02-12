06a2f654188a88074a96a71f7f564efbbb86ed94
se030 <se030.kim@gmail.com>
2023-02-13 01:48:41 +0900

    config: 절대경로 관련 설정 추가
    - `@constants` 경로 추가
    - storybook main.cjs에 vite.config.ts 설정 추가
    - https://github.com/storybookjs/storybook/issues/18891
    
    Co-authored-by: hodun <dohun31@users.noreply.github.com>
    Co-authored-by: Hyunbin Lee <iyu88@users.noreply.github.com>
    Co-authored-by: 이우재 <prayinforrain@users.noreply.github.com>

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,3 +1,8 @@
+const viteTsconfig = require('vite-tsconfig-paths');
+const tsconfigPaths = viteTsconfig.default;
+
+const { mergeConfig } = require('vite');
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -11,5 +16,10 @@ module.exports = {
   },
   features: {
     storyStoreV7: true,
+  },
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      plugins: [tsconfigPaths()],
+    });
   },
 };
