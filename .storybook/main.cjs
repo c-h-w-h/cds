4a66043059364025db6a377d25caf2545205f2ce
se030 <se030.kim@gmail.com>
2023-02-12 23:43:15 +0900

    style: eslint 규칙 수정 및 적용
    Co-authored-by: Hyunbin Lee <iyu88@users.noreply.github.com>
    Co-authored-by: hodun <dohun31@users.noreply.github.com>

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,13 +1,13 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  framework: "@storybook/react",
+  framework: '@storybook/react',
   core: {
-    builder: "@storybook/builder-vite",
+    builder: '@storybook/builder-vite',
   },
   features: {
     storyStoreV7: true,
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { resolve } from 'path';
 
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
