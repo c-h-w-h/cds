bb90f9b902cb8a1a8f1cfd0f4ffe3bf29247fd13
se030 <se030.kim@gmail.com>
2023-02-16 18:07:08 +0900

    feat: 컬러 팔레트 상수화

---
+++ src/constants/color.ts
@@ -0,0 +1,15 @@
+export const COLOR = {
+  primary100: '#1493FF',
+  primary200: '#0075DC',
+  primary300: '#0058BA',
+  primary400: '#003D99',
+  primary500: '#002579',
+
+  error: '#E6382F',
+
+  gray100: '#E0E0E0',
+  gray200: '#D3D3D3',
+  black: '#333333',
+  white: '#FEFEFE',
+  offwhite: '#FCFCFC',
+} as const;
