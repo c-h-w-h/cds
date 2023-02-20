fa305adcfbf20d7987ae85c195f1c306e22167c2
sunmin <polarlsm1231@gmail.com>
2023-02-20 22:17:36 +0900

    fix: color theme 적용

--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -1,27 +1,30 @@
 import styled from '@emotion/styled';
 
 interface SpinnerProps {
-  size: string;
-  color: string;
+  size: 'small' | 'large';
 }
 
-const Spinner = ({ size = 'small', color = '#1493FF' }: SpinnerProps) => {
+const Spinner = ({ size = 'small' }: SpinnerProps) => {
   return (
     <SpinnerContainer {...{ size }}>
       <Spin>
-        <Circle {...{ size, color }} />
+        <Circle {...{ size }} />
       </Spin>
     </SpinnerContainer>
   );
 };
 
-const SpinnerContainer = styled.div<{ size: string }>(({ size }) => ({
-  width: size === 'small' ? '1.875rem' : '3.125rem',
-  height: size === 'small' ? '1.875rem' : '3.125rem',
-  display: 'inline-block',
-  overflow: 'hidden',
-  background: '#ffffff',
-}));
+const SpinnerContainer = styled.div<SpinnerProps>(({ theme, size }) => {
+  const { color: themeColor } = theme;
+  const { white } = themeColor;
+  return {
+    width: size === 'small' ? '1.875rem' : '3.125rem',
+    height: size === 'small' ? '1.875rem' : '3.125rem',
+    display: 'inline-block',
+    overflow: 'hidden',
+    background: white,
+  };
+});
 
 const Spin = styled.div`
   width: 100%;
@@ -40,17 +43,23 @@ const Spin = styled.div`
   }
 `;
 
-const Circle = styled.div<SpinnerProps>(({ size, color }) => ({
-  position: 'absolute',
-  width: size === 'small' ? '0.938rem' : '1.563rem',
-  height: size === 'small' ? '0.938rem' : '1.563rem',
-  border:
-    size === 'small' ? `0.125rem solid ${color}` : `0.188rem solid ${color}`,
-  borderTopColor: 'transparent',
-  borderRadius: '50%',
-  animation: 'spin-animation 1s linear infinite',
-  top: size === 'small' ? '0.938rem' : '1.563rem',
-  left: size === 'small' ? '0.938rem' : '1.563rem',
-}));
+const Circle = styled.div<SpinnerProps>(({ theme, size }) => {
+  const { color: themeColor } = theme;
+  const { primary100 } = themeColor;
+  return {
+    position: 'absolute',
+    width: size === 'small' ? '0.938rem' : '1.563rem',
+    height: size === 'small' ? '0.938rem' : '1.563rem',
+    border:
+      size === 'small'
+        ? `0.125rem solid ${primary100}`
+        : `0.188rem solid ${primary100}`,
+    borderTopColor: 'transparent',
+    borderRadius: '50%',
+    animation: 'spin-animation 1s linear infinite',
+    top: size === 'small' ? '0.938rem' : '1.563rem',
+    left: size === 'small' ? '0.938rem' : '1.563rem',
+  };
+});
 
 export default Spinner;
