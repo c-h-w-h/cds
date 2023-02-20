8a2919106193f49bec7e2e38699e1cc6450dc445
se030 <se030.kim@gmail.com>
2023-02-20 18:57:13 +0900

    feat: Container.stories 추가

---
+++ src/components/Container/Container.stories.tsx
@@ -0,0 +1,44 @@
+import { css } from '@emotion/react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Container from '.';
+
+export default {
+  title: 'Container',
+  component: Container,
+  parameters: {
+    layout: 'fullscreen',
+  },
+} as ComponentMeta<typeof Container>;
+
+const Template: ComponentStory<typeof Container> = (args) => (
+  <Container {...args} />
+);
+
+export const BasicContainer = Template.bind({});
+BasicContainer.args = {
+  css: css`
+    width: 300px;
+    height: 300px;
+  `,
+  children: (
+    <Container
+      css={css`
+        background-color: pink;
+      `}
+    >
+      <p>빈 공간이 많은 컨테이너</p>
+    </Container>
+  ),
+};
+
+export const ScrollContainer = Template.bind({});
+ScrollContainer.args = {
+  overflowY: 'scroll',
+  css: css`
+    height: 100px;
+  `,
+  children: (
+    <div style={{ height: '300px' }}>Container보다 세로로 더 긴 컨텐츠</div>
+  ),
+};
