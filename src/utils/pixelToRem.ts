cde58f60021d8c35c8853d8b53c51f94802d8484
se030 <se030.kim@gmail.com>
2023-02-27 10:09:18 +0900

    feat: 0.25rem 단위로 근사치 설정

--- src/utils/pixelToRem.ts
+++ src/utils/pixelToRem.ts
@@ -1,16 +1,32 @@
 export const pixelToRem = (str: PixelString) => {
   const tokens = str.split(/(\d+px)/);
 
-  const converter = (pre: string, cur: string) => {
+  const reducer = (pre: string, cur: string) => {
     if (/\d+px/.test(cur)) {
       const value = Number(/\d+/.exec(cur));
-      return pre + `${value / 16}rem`;
+
+      return pre + `${converter(value)}rem`;
     }
 
     return pre + cur;
   };
 
-  return tokens.reduce(converter, '');
+  return tokens.reduce(reducer, '');
+};
+
+const converter = (value: number) => {
+  const mod = value % 16;
+
+  switch (true) {
+    case mod < 4:
+      return Math.floor(value / 16);
+    case mod >= 4 && mod < 8:
+      return Math.floor(value / 16) + 0.25;
+    case mod >= 12:
+      return Math.ceil(value / 16) - 0.25;
+    default:
+      return Math.ceil(value / 16) - 0.5;
+  }
 };
 
 type PixelString = `${Pre}${number}px${Post}`;
