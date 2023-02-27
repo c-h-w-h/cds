9a471d6795d1dd0a67338eadceb633ef7d811e5b
sunmin <polarlsm1231@gmail.com>
2023-02-27 11:25:28 +0900

    refactor: style 코드 통일

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,7 +1,6 @@
 import Center from '@components/@layout/Center';
 import Flexbox from '@components/@layout/Flexbox';
 import Typography from '@components/Typography';
-import { TypographyVariant } from '@constants/typography';
 import styled from '@emotion/styled';
 import { DefaultPropsWithChildren } from '@utils/types/DefaultPropsWithChildren';
 import { MouseEventHandler } from 'react';
@@ -20,11 +19,7 @@ const Modal = ({ title, children, isOpen, onClose, actions }: ModalProps) => {
       <BackGround onClick={onClose}></BackGround>
       <Center>
         <ModalBox>
-          {title && (
-            <Typography variant={TypographyVariant.SUBTITLE1}>
-              {title}
-            </Typography>
-          )}
+          {title && <Typography variant="subtitle1">{title}</Typography>}
           <Content>{children}</Content>
           {actions && (
             <Footer>
@@ -53,20 +48,16 @@ const ModalWrapper = styled.div<Pick<ModalProps, 'isOpen'>>`
   justify-content: center;
 `;
 
-const ModalBox = styled.div(({ theme }) => {
-  const { color: themeColor } = theme;
-  const { white } = themeColor;
-  return {
-    width: '20rem',
-    height: 'min-content',
-    position: 'relative',
-    zIndex: 999,
-    padding: '1rem 0',
-    boxShadow: 'rgb(0 0 0 / 20%) 0px 2px 5px 1px',
-    borderRadius: '1rem',
-    backgroundColor: white,
-  };
-});
+const ModalBox = styled.div`
+  width: 20rem;
+  height: min-content;
+  position: relative;
+  z-index: 999;
+  padding: 1rem 0;
+  box-shadow: rgb(0 0 0 / 20%) 0px 2px 5px 1px;
+  border-radius: 1rem;
+  background-color: ${({ theme }) => theme.color.white};
+`;
 
 const Content = styled.div`
   height: min-content;
@@ -79,45 +70,37 @@ const Footer = styled.div`
   justify-content: space-between;
 `;
 
-const BackGround = styled.div(({ theme }) => {
-  const { color: themeColor } = theme;
-  const { black } = themeColor;
-  return {
-    width: '100%',
-    height: '100%',
-    position: 'fixed',
-    zIndex: 998,
-    display: 'flex',
-    opacity: 0.4,
-    backgroundColor: black,
-  };
-});
+const BackGround = styled.div`
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  z-index: 998;
+  display: flex;
+  opacity: 0.4;
+  background-color: ${({ theme }) => theme.color.black};
+`;
 
 /**
  * 임시 Button style
  * 후에 구현된 Button 컴포넌트로 바꿀 예정
  */
-export const Button = styled.button(({ theme }) => {
-  const { color: themeColor } = theme;
-  const { primary100, white } = themeColor;
-  return {
-    width: 'min-content',
-    height: '1.7rem',
-    padding: '0 1.2rem',
-    borderRadius: '0.5rem',
-    backgroundColor: primary100,
-    color: white,
-    whiteSpace: 'nowrap',
-    cursor: 'pointer',
-    '@media (hover: hover)': {
-      ':hover': {
-        filter: 'brightness(0.9)',
-      },
-    },
-    ':active': {
-      filter: 'brightness(0.7)',
-    },
-  };
-});
+export const Button = styled.button`
+  width: min-content;
+  height: 1.7rem;
+  padding: 0 1.2rem;
+  border-radius: 0.5rem;
+  background-color: ${({ theme }) => theme.color.primary100};
+  color: ${({ theme }) => theme.color.white};
+  white-space: nowrap;
+  cursor: pointer;
+  @media (hover: hover) {
+    &:hover {
+      filter: brightness(0.9);
+    }
+  }
+  &:active {
+    filter: brightness(0.7);
+  }
+`;
 
 export default Modal;
