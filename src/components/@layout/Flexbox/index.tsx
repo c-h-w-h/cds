50987f2bee65bb38d49da9eb42d82020107e419a
se <63814960+se030@users.noreply.github.com>
2023-03-06 15:30:30 +0900

    Feat/button: Button 컴포넌트 구현 (#47)
    * fix: pixelToRem 메서드 개선
    
    - 사용처에서 px이 포함된 문자열인지 알 수 없는 경우가 있어(e.g. CSSProperties['width']) 파라미터 타입을 가드로 대체
    - border 1px 등을 사용하고 싶은 경우 0rem을 반환함
    - 그 외에도 0.25rem 단위로는 정교한 스타일링을 할 수 없어 2px 단위로 올림 처리하도록 변경
    
    * feat: Button 컴포넌트 구현
    
    * feat: Icon 컴포넌트 구현
    
    - 용례는 abbb6ccfe0f171938680e8ea272792afb6a1936b Button 컴포넌트 참고
    - react-icons 패키지의 아이콘 단독으로 사용할 때는 감쌀 필요 없음
    
    * feat: style='square' 스타일 수정 및 스토리 추가
    
    * fix: 닫는 중괄호 추가
    
    * feat: disabled 스타일링 추가
    
    * fix: 기본 속성과 중복되는 style 네이밍 variant로 변경
    
    * feat: flexboxStyle 추가
    
    - flexboxStyle 분리, 추후 Flexbox 컴포넌트에 반영 예정
    - 레이아웃 컴포넌트는 사용되는 태그의 HTML 기본 속성들을 사용할 수 있어야 함(이벤트 핸들러, button - type, disabled, a - href 등)
    - Flexbox 컴포넌트와 as, withComponent만으로 타입 확장되게 구현하기가 어려움
    
    * feat: 버튼이 링크로 사용되는 케이스 추가
    
    * fix: 필요없는 eslint-disable-next-line 제거
    
    * fix: 변수명 오타 수정
    
    * feat: a 태그 text-decoration none으로 캐스캐이드
    
    * refactor: ButtonVariant 타입 정의 수정
    
    * feat: disabled props 및 스토리 추가
    
    * fix: 리뷰 반영
    
    - getBorderRadius 가독성 개선
    - 불필요한 pixelToRem 제거
    
    * refactor: flexboxStyle Flexbox에 반영
    
    * fix: 스타일 수정

--- src/components/@layout/Flexbox/index.tsx
+++ src/components/@layout/Flexbox/index.tsx
@@ -1,31 +1,13 @@
 import styled from '@emotion/styled';
+import { flexboxStyle } from '@styles/flex-box';
 import { DefaultPropsWithChildren } from '@util-types/DefaultPropsWithChildren';
 import { FlexContainerProps } from '@util-types/FlexContainerProps';
 
 type FlexboxProps = DefaultPropsWithChildren<HTMLDivElement> &
   FlexContainerProps;
 
-const Flexbox = styled.div<FlexboxProps>(
-  ({
-    flexDirection = 'row',
-    flexWrap = 'nowrap',
-    alignContent = 'normal',
-    alignItems = 'center',
-    justifyContent = 'center',
-    gap = '1rem',
-  }) => {
-    return [
-      {
-        display: 'flex',
-        flexDirection,
-        flexWrap,
-        alignContent,
-        alignItems,
-        justifyContent,
-        gap,
-      },
-    ];
-  },
-);
+const Flexbox = styled.div<FlexboxProps>((props) => {
+  return flexboxStyle(props);
+});
 
 export default Flexbox;
---
+++ src/components/Icon/index.tsx
@@ -0,0 +1,34 @@
+import styled from '@emotion/styled';
+import { IconType } from '@react-icons/all-files';
+import { pixelToRem } from '@utils/pixelToRem';
+import { CSSProperties } from 'react';
+
+export type IconSource = string | IconType;
+
+interface IconProps {
+  source: IconSource;
+  size: CSSProperties['width'];
+  color: CSSProperties['fill'];
+}
+
+const Icon = ({ source: Source, size, color }: IconProps) => {
+  if (typeof Source === 'string') {
+    return (
+      <IconContainer
+        src={Source}
+        width={pixelToRem(`${size}`)}
+        height={pixelToRem(`${size}`)}
+      />
+    );
+  }
+
+  return <Source size={size} color={color} />;
+};
+
+const IconContainer = styled.img`
+  max-width: 100%;
+  max-height: 100%;
+  aspect-ratio: 1 / 1;
+`;
+
+export default Icon;
