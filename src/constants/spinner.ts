4c925c2a54095622585788321bce38108087abc4
se <63814960+se030@users.noreply.github.com>
2023-03-06 13:55:41 +0900

    Refactor/enum-types: enum 기반 타입 string literal로 변경 (#50)
    * refactor: enum 기반 타입 string literal로 변경
    
    * fix: import문 자동완성 위해 절대경로 순서 변경

--- src/constants/spinner.ts
+++ src/constants/spinner.ts
@@ -1,21 +1,16 @@
-export enum SpinnerSizeVariant {
-  SMALL = 'small',
-  LARGE = 'large',
-}
-
-export interface SpinDesignVariant {
+interface SpinnerStyle {
   OUTCIRCLE: string;
   INNERCIRCLE: string;
   STROKEWIDTH: string;
 }
 
-export const SPINNER_SIZE: Record<SpinnerSizeVariant, SpinDesignVariant> = {
-  [SpinnerSizeVariant.SMALL]: {
+export const SPINNER_STYLE: Record<SpinnerSizeVariant, SpinnerStyle> = {
+  small: {
     OUTCIRCLE: '1.875rem',
     INNERCIRCLE: '0.938rem',
     STROKEWIDTH: '0.125rem',
   },
-  [SpinnerSizeVariant.LARGE]: {
+  large: {
     OUTCIRCLE: '3.125rem',
     INNERCIRCLE: '1.563rem',
     STROKEWIDTH: '0.188rem',
