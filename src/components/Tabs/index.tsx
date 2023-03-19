cffd07a936dce45ab34797b00d54bb5052606953
iyu88 <iyu88@naver.com>
2023-03-20 02:21:29 +0900

    feat: TabTrigger CSS 속성 추가
    Tabs에 Props로 전달하는 값 중 variant 값에 따라 디자인이 달라지도록 CSS를 설정합니다.
    Theme의 영향을 받는 객체를 variant 값에 따라 CSS props에 전달합니다.

--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -1,11 +1,15 @@
 import Button from '@components/Button';
 import Container from '@components-layout/Container';
 import Flexbox from '@components-layout/Flexbox';
-import { css } from '@emotion/react';
+import { css, useTheme } from '@emotion/react';
 import { createContext, ReactNode, useContext, useState } from 'react';
+
+type TabListVariant = 'underline' | 'rounded';
 
 const TabsContext =
   createContext<{
+    isFitted: boolean;
+    variant: TabListVariant;
     selectedIndex: string;
     setSelectedIndex: React.Dispatch<React.SetStateAction<string>>;
   } | null>(null);
@@ -13,13 +17,22 @@ const TabsContext =
 interface TabsProps {
   defaultValue: string;
   children: ReactNode;
+  variant?: TabListVariant;
+  isFitted?: boolean;
 }
 
-const Tabs = ({ defaultValue, children }: TabsProps) => {
+const Tabs = ({
+  isFitted = false,
+  variant = 'underline',
+  defaultValue,
+  children,
+}: TabsProps) => {
   const [selectedIndex, setSelectedIndex] = useState<string>(defaultValue);
 
+  const providerValue = { isFitted, variant, selectedIndex, setSelectedIndex };
+
   return (
-    <TabsContext.Provider value={{ selectedIndex, setSelectedIndex }}>
+    <TabsContext.Provider value={providerValue}>
       <Container
         css={css`
           padding: 1rem;
@@ -33,20 +46,27 @@ const Tabs = ({ defaultValue, children }: TabsProps) => {
 
 interface TabListProps {
   children: ReactNode;
-  isFitted?: boolean;
 }
 
-const List = ({ isFitted = false, children }: TabListProps) => {
+const List = ({ children }: TabListProps) => {
+  const context = useContext(TabsContext);
+  const { color: themeColor } = useTheme();
+  const { white, gray100 } = themeColor;
+
+  if (context === null) return null;
   return (
     <Container overflowX="auto">
       <Flexbox
         justifyContent={'flex-start'}
         css={css`
-          width: ${isFitted === false && 'max-content'};
+          gap: 0;
+          width: 100%;
+          border-bottom: 2px solid ${gray100};
 
           & > button {
-            width: ${isFitted === true && '100%'};
+            width: ${context.isFitted === true && '100%'};
             white-space: nowrap;
+            background-color: ${white};
           }
         `}
       >
@@ -64,13 +84,50 @@ interface TabTriggerProps {
 
 const Trigger = ({ value, disabled = false, children }: TabTriggerProps) => {
   const context = useContext(TabsContext);
-
   if (context === null) return null;
+
+  const { color: themeColor } = useTheme();
+  const { primary100, gray100, black, white } = themeColor;
+
+  const underlineStyle = {
+    'border-color': `${context.selectedIndex === value ? primary100 : gray100}`,
+    'border-radius': 0,
+    'border-bottom-width': '2px',
+    'border-bottom-style': 'solid',
+    'margin-bottom': '-2px',
+  } as const;
+
+  const roundedStyle = {
+    border: `${context.selectedIndex === value && `2px ${gray100} solid`}`,
+    'border-radius': '10px',
+    'border-bottom-left-radius': 0,
+    'border-bottom-right-radius': 0,
+    'border-bottom-color': `${white}`,
+    'margin-bottom': '-2px',
+  } as const;
+
+  const triggerStyles = {
+    underline: underlineStyle,
+    rounded: roundedStyle,
+  };
+
   return (
     <Button
       text={children?.toString()}
       disabled={disabled}
       onClick={() => context.setSelectedIndex(value)}
+      css={css`
+        ${triggerStyles[context.variant]}
+
+        & > p {
+          color: ${context.selectedIndex === value ? primary100 : black};
+        }
+
+        &:hover {
+          background-color: ${primary100};
+          border-bottom-color: ${primary100};
+        }
+      `}
     />
   );
 };
