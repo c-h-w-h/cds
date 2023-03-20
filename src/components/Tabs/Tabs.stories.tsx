ebc1764bd86f8f807dded22a8e60c9fb872bca8e
Hyunbin Lee <31645195+iyu88@users.noreply.github.com>
2023-03-20 12:43:12 +0900

    Feat/tabs: Tabs 컴포넌트 구현  (#63)
    * feat: Tabs 컴포넌트 기본 구조 잡기
    
    * feat: TabList 에 Button 컴포넌트 적용
    
    * feat: TabList isFitted 속성 추가
    
    isFitted 속성을 추가하여 한 화면 안에 들어올 수 있는 정도의 Tab 개수가 있을 경우 화면에 맞춰서 렌더링할 수 있게 합니다.
    만약 개수가 너무 많은 경우 좌우 스크롤이 가능하도록 합니다.
    
    * feat: TabTrigger CSS 속성 추가
    
    Tabs에 Props로 전달하는 값 중 variant 값에 따라 디자인이 달라지도록 CSS를 설정합니다.
    Theme의 영향을 받는 객체를 variant 값에 따라 CSS props에 전달합니다.
    
    * fix: 스타일 Object 속성을 케밥 케이스로 수정
    
    * feat: Trigger에서 icon 속성 지원
    
    icon 속성 값을 전달할 경우 Trigger 내부에서 사용하는 Button 컴포넌트로 넘겨서 아이콘을 렌더링합니다.
    
    * refactor: TabsVariant 타입 이름 수정
    
    * refactor: TabContent 를 TabPanel 로 이름 변경
    
    Tab의 MDN 접근성 문서를 참고하여 Tab의 내용이 나타나는 부분의 이름을 Panel로 변경합니다.
    
    관련 링크
    - https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/tabpanel_role
    
    * feat: Trigger disabled 시 CSS 스타일링
    
    * feat: Tabs 스토리 작성
    
    * feat: 스크롤 관련 기능 및 스토리 추가
    
    Trigger 가 많을 경우 스크롤이 가능함을 보여주는 스토리를 추가합니다.
    스크롤이 보이지 않도록 CSS 속성을 추가해줍니다.
    
    * refactor: emotions CSS 포맷 수정
    
    * refactor: TabsContextInterface 분리

---
+++ src/components/Tabs/Tabs.stories.tsx
@@ -0,0 +1,135 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { MdCelebration, MdInfo, MdCheckCircle } from 'react-icons/md';
+
+import Tabs from '.';
+
+export default {
+  title: 'Tabs',
+  component: Tabs,
+  parameters: {
+    layout: 'fullscreen',
+  },
+} as ComponentMeta<typeof Tabs>;
+
+const DUMMY_STR_1 =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Est velit egestas dui id ornare arcu odio. Aliquet nibh praesent tristique magna sit. Mollis aliquam ut porttitor leo a diam sollicitudin. Diam maecenas sed enim ut sem viverra aliquet. Lorem ipsum dolor sit amet consectetur adipiscing. Lacinia at quis risus sed vulputate odio ut. Faucibus ornare suspendisse sed nisi lacus sed viverra. Vulputate enim nulla aliquet porttitor. Nunc vel risus commodo viverra maecenas. Nec feugiat in fermentum posuere urna nec tincidunt praesent semper. Netus et malesuada fames ac turpis egestas maecenas pharetra convallis. Condimentum vitae sapien pellentesque habitant morbi tristique senectus et netus. Lacus vestibulum sed arcu non odio euismod. Elit pellentesque habitant morbi tristique senectus et netus et. Dui accumsan sit amet nulla facilisi morbi tempus. Libero nunc consequat interdum varius sit amet mattis vulputate. Ultricies integer quis auctor elit sed vulputate mi. Aliquam purus sit amet luctus.';
+const DUMMY_STR_2 =
+  'Ut porttitor leo a diam sollicitudin tempor. Mattis aliquam faucibus purus in massa tempor nec feugiat. Congue eu consequat ac felis. Sed adipiscing diam donec adipiscing tristique risus nec feugiat in. Euismod in pellentesque massa placerat duis ultricies lacus sed turpis. Metus aliquam eleifend mi in nulla posuere sollicitudin. Arcu dui vivamus arcu felis bibendum ut tristique. Magna etiam tempor orci eu lobortis elementum nibh tellus. Euismod nisi porta lorem mollis aliquam ut porttitor leo a. Nisl tincidunt eget nullam non nisi est sit amet. Ut consequat semper viverra nam libero justo laoreet sit. Quis risus sed vulputate odio ut enim. Tincidunt ornare massa eget egestas. Id cursus metus aliquam eleifend. Blandit cursus risus at ultrices mi. Arcu cursus vitae congue mauris rhoncus aenean vel. Arcu risus quis varius quam quisque id.';
+const DUMMY_STR_3 =
+  'Enim diam vulputate ut pharetra sit amet. Amet mauris commodo quis imperdiet massa tincidunt nunc. Venenatis cras sed felis eget velit aliquet. Suspendisse interdum consectetur libero id. Ut tortor pretium viverra suspendisse potenti nullam ac tortor vitae. Sit amet cursus sit amet dictum sit amet justo donec. Consequat mauris nunc congue nisi vitae. Cras tincidunt lobortis feugiat vivamus at augue eget arcu. Nullam eget felis eget nunc lobortis mattis. Tortor posuere ac ut consequat. Ornare arcu odio ut sem. Ante metus dictum at tempor commodo ullamcorper a lacus. Ut porttitor leo a diam sollicitudin. At imperdiet dui accumsan sit amet nulla facilisi morbi tempus. Mattis molestie a iaculis at erat. Ut eu sem integer vitae justo eget magna fermentum iaculis. Quam adipiscing vitae proin sagittis nisl.';
+
+const Template: ComponentStory<typeof Tabs> = (args) => (
+  <Tabs {...args}>
+    <Tabs.List>
+      <Tabs.Trigger value="1">메인</Tabs.Trigger>
+      <Tabs.Trigger value="2">이벤트</Tabs.Trigger>
+      <Tabs.Trigger value="3">설정</Tabs.Trigger>
+    </Tabs.List>
+    <Tabs.Panel value="1">
+      <span>첫번째 탭</span>
+      <div>{DUMMY_STR_1}</div>
+    </Tabs.Panel>
+    <Tabs.Panel value="2">
+      <span>두번째 탭</span>
+      <div>{DUMMY_STR_2}</div>
+    </Tabs.Panel>
+    <Tabs.Panel value="3">
+      <span>세번째 탭</span>
+      <div>{DUMMY_STR_3}</div>
+    </Tabs.Panel>
+  </Tabs>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  defaultValue: '1',
+};
+
+export const Rounded = Template.bind({});
+Rounded.args = {
+  defaultValue: '1',
+  variant: 'rounded',
+};
+
+export const UnderlineFitted = Template.bind({});
+UnderlineFitted.args = {
+  defaultValue: '1',
+  variant: 'underline',
+  isFitted: true,
+};
+
+export const RoundedFitted = Template.bind({});
+RoundedFitted.args = {
+  defaultValue: '1',
+  variant: 'rounded',
+  isFitted: true,
+};
+
+export const DefaultValue = Template.bind({});
+DefaultValue.args = {
+  defaultValue: '3',
+};
+
+const WithIconsTemplate: ComponentStory<typeof Tabs> = (args) => (
+  <Tabs {...args}>
+    <Tabs.List>
+      <Tabs.Trigger value="1" icon={MdCelebration}>
+        축하
+      </Tabs.Trigger>
+      <Tabs.Trigger value="2" icon={MdInfo}>
+        정보
+      </Tabs.Trigger>
+      <Tabs.Trigger value="3" icon={MdCheckCircle}>
+        확인
+      </Tabs.Trigger>
+    </Tabs.List>
+  </Tabs>
+);
+
+export const WithIcons = WithIconsTemplate.bind({});
+WithIcons.args = {
+  defaultValue: '1',
+};
+
+const WithDisabledTemplate: ComponentStory<typeof Tabs> = (args) => (
+  <Tabs {...args}>
+    <Tabs.List>
+      <Tabs.Trigger value="1" icon={MdCelebration}>
+        축하
+      </Tabs.Trigger>
+      <Tabs.Trigger value="2" icon={MdInfo} disabled>
+        정보
+      </Tabs.Trigger>
+      <Tabs.Trigger value="3" icon={MdCheckCircle}>
+        확인
+      </Tabs.Trigger>
+    </Tabs.List>
+  </Tabs>
+);
+
+export const WithDisabled = WithDisabledTemplate.bind({});
+WithDisabled.args = {
+  defaultValue: '1',
+};
+
+const ScrollableTemplate: ComponentStory<typeof Tabs> = (args) => (
+  <Tabs {...args}>
+    <Tabs.List>
+      <Tabs.Trigger value="1">
+        축하하는 탭을 띄우기 위한 트리거입니다.
+      </Tabs.Trigger>
+      <Tabs.Trigger value="2">
+        정보를 표시하기 위해서 여기를 클릭하세요.
+      </Tabs.Trigger>
+      <Tabs.Trigger value="3">아무튼 길어지면 스크롤이 생기겠죠?</Tabs.Trigger>
+      <Tabs.Trigger value="4">
+        여기까지 보이시면 너비를 줄여주시겠어요?
+      </Tabs.Trigger>
+    </Tabs.List>
+  </Tabs>
+);
+
+export const Scrollable = ScrollableTemplate.bind({});
+Scrollable.args = {
+  defaultValue: '1',
+};
---
+++ src/components/Tabs/index.tsx
@@ -0,0 +1,210 @@
+import Button from '@components/Button';
+import { IconSource } from '@components/Icon';
+import Container from '@components-layout/Container';
+import Flexbox from '@components-layout/Flexbox';
+import { css, useTheme } from '@emotion/react';
+import { pixelToRem } from '@utils/pixelToRem';
+import {
+  createContext,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useContext,
+  useState,
+} from 'react';
+
+type TabsVariant = 'underline' | 'rounded';
+
+interface TabsContextInterface {
+  isFitted: boolean;
+  variant: TabsVariant;
+  selectedIndex: string;
+  setSelectedIndex: Dispatch<SetStateAction<string>>;
+}
+
+const TabsContext = createContext<TabsContextInterface | null>(null);
+
+interface TabsProps {
+  defaultValue: string;
+  children: ReactNode;
+  variant?: TabsVariant;
+  isFitted?: boolean;
+}
+
+const Tabs = ({
+  isFitted = false,
+  variant = 'underline',
+  defaultValue,
+  children,
+}: TabsProps) => {
+  const [selectedIndex, setSelectedIndex] = useState<string>(defaultValue);
+
+  const providerValue = { isFitted, variant, selectedIndex, setSelectedIndex };
+
+  return (
+    <TabsContext.Provider value={providerValue}>
+      <Container
+        css={css`
+          padding: 1rem;
+        `}
+      >
+        {children}
+      </Container>
+    </TabsContext.Provider>
+  );
+};
+
+interface TabListProps {
+  children: ReactNode;
+}
+
+const List = ({ children }: TabListProps) => {
+  const context = useContext(TabsContext);
+  const { color: themeColor } = useTheme();
+  const { white, gray100 } = themeColor;
+
+  if (context === null) return null;
+  return (
+    <Container
+      overflowX="auto"
+      css={css`
+        -ms-overflow-style: none;
+
+        ::-webkit-scrollbar {
+          display: none;
+        }
+      `}
+    >
+      <Flexbox
+        justifyContent={'flex-start'}
+        css={css`
+          gap: 0;
+          width: 100%;
+          border-bottom: 2px solid ${gray100};
+
+          & > button {
+            width: ${context.isFitted === true && '100%'};
+            white-space: nowrap;
+            background-color: ${white};
+          }
+        `}
+      >
+        {children}
+      </Flexbox>
+    </Container>
+  );
+};
+
+interface TabTriggerProps {
+  value: string;
+  children: ReactNode;
+  disabled?: boolean;
+  icon?: IconSource;
+}
+
+const Trigger = ({
+  value,
+  disabled = false,
+  icon,
+  children,
+}: TabTriggerProps) => {
+  const context = useContext(TabsContext);
+  if (context === null) return null;
+
+  const { color: themeColor } = useTheme();
+  const { primary100, gray100, black, white } = themeColor;
+  const isActive = context.selectedIndex === value;
+
+  const underlineStyle = {
+    borderColor: `${context.selectedIndex === value ? primary100 : gray100}`,
+    borderRadius: 0,
+    borderBottomWidth: '2px',
+    borderBottomStyle: 'solid',
+    marginBottom: '-2px',
+  } as const;
+
+  const roundedStyle = {
+    border: `${context.selectedIndex === value && `2px ${gray100} solid`}`,
+    borderRadius: '10px',
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0,
+    borderBottomColor: `${white}`,
+    marginBottom: '-2px',
+  } as const;
+
+  const triggerStyles = {
+    underline: underlineStyle,
+    rounded: roundedStyle,
+  };
+
+  return (
+    <Button
+      text={children?.toString()}
+      disabled={disabled}
+      icon={icon}
+      iconSize={pixelToRem('16px')}
+      onClick={() => !disabled && context.setSelectedIndex(value)}
+      css={css`
+        ${triggerStyles[context.variant]}
+
+        & > p {
+          color: ${isActive ? primary100 : black};
+        }
+
+        & > svg {
+          fill: ${isActive ? primary100 : black};
+        }
+
+        &:hover {
+          background-color: ${primary100};
+          border-bottom-color: ${primary100};
+        }
+
+        &:disabled {
+          background-color: ${white};
+          cursor: not-allowed;
+
+          & > p {
+            color: ${gray100};
+          }
+
+          & > svg {
+            fill: ${gray100};
+          }
+
+          &:hover {
+            border-bottom-color: ${gray100};
+          }
+        }
+      `}
+    />
+  );
+};
+
+interface TabPanelProps {
+  value: string;
+  children: ReactNode;
+}
+
+const Panel = ({ value, children }: TabPanelProps) => {
+  const context = useContext(TabsContext);
+
+  if (context === null) return null;
+
+  return (
+    <Container
+      css={css`
+        padding: 1rem;
+        display: ${context.selectedIndex === value ? 'block' : 'none'};
+      `}
+    >
+      {children}
+    </Container>
+  );
+};
+
+Tabs.List = List;
+Tabs.Trigger = Trigger;
+Tabs.Panel = Panel;
+
+export default Tabs;
