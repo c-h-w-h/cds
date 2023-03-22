1d1a5d80b93964a4569634572f6a5d24a544d1fc
se <63814960+se030@users.noreply.github.com>
2023-03-22 23:34:01 +0900

    Feat/select: Select 컴포넌트 구현 (#65)
    * feat: flexboxStyle default value 지정
    
    * feat: Select 컴포넌트 기본 동작 구현
    
    - 시각적인 형태를 제외한 기본 동작만 구현
    - Trigger 요소는 Dropdown 컴포넌트 사용
    - Option 요소를 클릭하면 선택된 값이 바뀌고 해당 선택이 HiddenSelect 내 option 태그에 반영
    
    * feat: 옵션 선택했을 때 드롭다운 메뉴 닫기
    
    * feat: Select 스토리 추가
    
    * refactor: 커스텀훅 네이밍 변경
    
    감춰진 select 태그와 관련된 동작을 모듈화
    
    * fix: hiddenStyle 수정
    
    * fix: option 태그가 이미 존재하는 경우 early return
    
    * refactor: 리뷰 반영
    
    - setValue 타입 문자열로 고정: .stories 용례 참고
    - childNodes 스프레드 연산자 제거
    - Option에 hover style 추가
    
    * refactor: ChildrenProps 분리
    
    속성 props 제외하고 children만 원하는 경우 사용할 수 있게 분리
    
    * refactor: 리뷰 반영
    
    - 노드 변수명 $li, $option 으로 수정
    - context 존재하지 않는 경우 빈 Fragment 반환
    
    * docs: 스토리 설명 일부 추가
    
    * fix: DefaultProps에서 optionalChildren 제거
    
    - 현재 스펙에 children이 옵셔널이어야 하는 케이스는 없고, 오히려 Input 등의 컴포넌트가 optional children을 가질 수 있는 케이스가 잘못되어서 제거
    - 변경 사항에 따라 Dropdown 컴포넌트의 타입 일부 수정
    
    * fix: 수정된 DropdownProps에 맞게 id, dropdownLabel 추가

---
+++ src/components/Select/index.tsx
@@ -0,0 +1,109 @@
+import Dropdown, { DropdownContext } from '@components/Dropdown';
+import { css } from '@emotion/react';
+import { ChildrenProps } from '@util-types/ChildrenProps';
+import {
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  createContext,
+  forwardRef,
+  useContext,
+} from 'react';
+
+import useSelect from './useSelect';
+
+type SelectProps = {
+  id: string;
+  setValue?: Dispatch<SetStateAction<string>>;
+  children: ReactNode;
+};
+
+interface SelectContextInterface {
+  selectValue: (value: string) => void;
+  registerOption: ($li: HTMLLIElement, value: string) => void;
+}
+const SelectContext = createContext<SelectContextInterface | null>(null);
+
+const Select = ({ id, setValue, children }: SelectProps) => {
+  const { selectRef, selectValue, registerOption } = useSelect(id, setValue);
+
+  return (
+    <Dropdown
+      id={`${id}_select_dropdown`}
+      dropdownLabel={`Opens ${id} select`}
+      collapseOnBlur={true}
+    >
+      <SelectContext.Provider value={{ selectValue, registerOption }}>
+        {children}
+      </SelectContext.Provider>
+      <HiddenSelect id={id} ref={selectRef} />
+    </Dropdown>
+  );
+};
+
+const Options = ({ children }: ChildrenProps) => {
+  return (
+    <Dropdown.Menu>
+      <ul role="listbox">{children}</ul>
+    </Dropdown.Menu>
+  );
+};
+
+type OptionProps = {
+  value: string;
+} & ChildrenProps;
+
+const Option = ({ value, children }: OptionProps) => {
+  const context = useContext(SelectContext);
+  if (!context) return <></>;
+
+  const { selectValue, registerOption } = context;
+
+  const refCallback = ($li: HTMLLIElement | null) => {
+    if (!$li) return;
+    registerOption($li, value);
+  };
+
+  const dropdownContext = useContext(DropdownContext);
+  if (!dropdownContext) {
+    return <></>;
+  }
+  const { setIsOpen } = dropdownContext;
+
+  const onSelect = () => {
+    selectValue(value);
+    setIsOpen(false);
+  };
+
+  return (
+    <li
+      role="listitem"
+      ref={refCallback}
+      onClick={onSelect}
+      css={css`
+        cursor: pointer;
+      `}
+    >
+      {children}
+    </li>
+  );
+};
+
+const HiddenSelect = forwardRef<HTMLSelectElement, { id: string }>(
+  ({ id }, ref) => {
+    return <select id={id} name={id} ref={ref} css={hiddenStyle}></select>;
+  },
+);
+const hiddenStyle = css`
+  position: absolute;
+  visibility: hidden;
+  height: 0px;
+  width: 0px;
+`;
+HiddenSelect.displayName = 'HiddenSelect';
+
+Select.Trigger = Dropdown.Trigger;
+Select.Options = Options;
+Select.Option = Option;
+
+export default Select;
