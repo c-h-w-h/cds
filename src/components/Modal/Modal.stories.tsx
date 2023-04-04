6d5b63b24566fbdfc9a5068726507c6ca297c68b
sunmin <polarlsm1231@gmail.com>
2023-04-04 13:56:58 +0900

    docs: modal storybook description 추가

--- src/components/Modal/Modal.stories.tsx
+++ src/components/Modal/Modal.stories.tsx
@@ -7,10 +7,25 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import Modal from '.';
 
 export default {
-  title: 'Modal',
+  title: 'Design System/Components/Modal',
   component: Modal,
   parameters: {
     layout: 'fullscreen',
+    componentSubtitle:
+      'Modal은 특정 컨텐츠가 담긴 화면을 다른 화면 위로 띄워줍니다.',
+  },
+  argTypes: {
+    isOpen: {
+      name: 'isOpen',
+      description: 'Modal을 띄울지 여부를 나타내는 상태입니다.',
+      table: {
+        type: { summary: false, required: true },
+      },
+    },
+    onClose: {
+      name: 'onClose',
+      description: 'Modal을 닫는 setState 함수입니다.',
+    },
   },
 } as ComponentMeta<typeof Modal>;
 
