3e67bde3f80616fbe703b8ec186524fe727bda79
se030 <se030.kim@gmail.com>
2023-04-11 14:51:51 +0900

    refactor: Button과 ButtonLink 컴포넌트 분리

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,110 +1,25 @@
-import { css, useTheme } from '@emotion/react';
-import { flexboxStyle } from '@styles/flex-box';
-import { DefaultProps } from '@utils/types/DefaultProps';
+import { ButtonHTMLAttributes } from 'react';
 
-interface ButtonProps extends DefaultProps<HTMLButtonElement> {
+import useButtonStyle from './useButtonStyle';
+
+export interface ButtonBaseProps {
   variant?: ButtonVariant;
   label: string;
-  href?: string;
-  // react.HTMLAttributes에 정의되지 않은 standard button attributes
-  type?: 'button' | 'submit' | 'reset';
-  disabled?: boolean;
   children: TextNode;
 }
+
+type ButtonProps = ButtonBaseProps & ButtonHTMLAttributes<HTMLButtonElement>;
 
 const Button = ({
   variant = 'square',
   label,
-  href,
-  type,
-  disabled,
   children,
   ...props
 }: ButtonProps) => {
-  const { color: themeColor } = useTheme();
-  const { white, primary200, primary400, gray200 } = themeColor;
-
-  const isPlain = variant === 'plain';
-  const hasBackground = !variant.includes('light') && !isPlain;
-  const isSquare = !variant.includes('round');
-
-  const containerStyle = css`
-    cursor: pointer;
-    width: fit-content;
-
-    &:disabled,
-    &.disabled {
-      cursor: not-allowed;
-    }
-
-    &,
-    * {
-      font-size: 1rem;
-      font-weight: 400;
-    }
-
-    svg,
-    img {
-      width: 1.5rem;
-      height: 1.5rem;
-    }
-
-    ${isPlain
-      ? css`
-          background: transparent;
-        `
-      : css`
-          border-radius: ${isSquare ? '8px' : '2.25rem'};
-          background-color: ${hasBackground ? primary200 : white};
-          border: 0.125rem solid ${primary200};
-
-          &:hover {
-            background-color: ${hasBackground ? primary400 : white};
-            border: 0.125rem solid ${primary400};
-          }
-
-          &:disabled,
-          &.disabled {
-            background-color: ${hasBackground ? gray200 : white};
-            border: 0.125rem solid ${gray200};
-          }
-        `}
-  `;
-
-  const contentStyle = css`
-    padding: 0.75rem;
-    color: ${hasBackground ? white : primary200};
-    text-decoration: none;
-
-    ${!hasBackground &&
-    css`
-      &:hover {
-        color: ${primary400};
-      }
-
-      &:disabled,
-      &.disabled {
-        color: ${gray200};
-      }
-    `}
-  `;
-
-  const commonProps: Record<string, unknown> = {
-    'aria-label': label,
-    css: [flexboxStyle({ gap: '0.125rem' }), containerStyle, contentStyle],
-    ...props,
-  };
-
-  if (href) {
-    return (
-      <a href={href} {...commonProps} className={disabled ? 'disabled' : ''}>
-        {children}
-      </a>
-    );
-  }
+  const { style } = useButtonStyle(variant);
 
   return (
-    <button {...commonProps} {...{ type, disabled }}>
+    <button css={style} aria-label={label} {...props}>
       {children}
     </button>
   );
