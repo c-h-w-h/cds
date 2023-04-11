84e956cf3f5c46fede3f9ee3caad29da1929b1af
se030 <se030.kim@gmail.com>
2023-04-11 15:00:24 +0900

    docs: 스토리 수정

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,3 +1,6 @@
+import ButtonLink from '@components/ButtonLink';
+import Icon from '@components/Icon';
+import Flexbox from '@components-layout/Flexbox';
 import { css } from '@emotion/react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { MdCelebration } from 'react-icons/md';
@@ -16,9 +19,8 @@ export default {
         component:
           '- variant 값으로 "round" | "square" | "light" | "round light" | "square light" | "plain" 중 하나를 선택할 수 있습니다.\n' +
           '\t- plain 버튼은 UI 개발 중 시맨틱 마크업을 위해 사용할 수 있습니다. background, border를 제외하고 인터랙션에 필요한 기본적인 스타일링을 제공합니다. \n' +
-          '- text, icon 값은 요구사항에 따라 선택적으로 사용이 가능합니다.\n' +
-          '- 링크 역할을 하는 버튼은 href props를 전달합니다.\n' +
-          '\t- 이 경우 버튼 컴포넌트의 디자인이 동일하게 적용되지만 시맨틱 마크업과 접근성을 위해 a 태그를 사용합니다.',
+          '- 링크 역할을 하는 버튼은 `ButtonLink` 컴포넌트 사용을 권장합니다.\n' +
+          '\t- 동일한 스타일링이 적용되지만 시맨틱 마크업 및 접근성을 포함한 네이티브 엘레먼트 기능 활용을 위해 `<a>` 태그를 사용합니다.',
       },
     },
   },
@@ -41,40 +43,11 @@ export default {
         ],
       },
     },
-    text: {
-      description: '버튼 내부에 들어갈 텍스트입니다.',
-      table: {
-        type: { summary: 'string' },
-      },
-    },
     label: {
       description:
         'aria-label 속성으로 사용되는 값입니다. 요소의 역할을 간단히 설명합니다.',
       table: {
         type: { summary: 'string' },
-      },
-    },
-    icon: {
-      description:
-        '버튼 내부에 들어갈 아이콘입니다. `react-icons` 라이브러리 아이콘 또는 URL string을 사용할 수 있습니다.',
-      table: {
-        type: { summary: 'IconType | string' },
-      },
-      control: false,
-    },
-    iconSize: {
-      description: '아이콘이 있는 경우 그 크기를 결정합니다.',
-      table: {
-        type: { summary: "CSSProperties['width']" },
-      },
-      control: {
-        type: 'number',
-      },
-    },
-    iconPosition: {
-      description: '아이콘이 있는 경우 그 위치를 결정합니다.',
-      table: {
-        type: { summary: '"left" | "right"' },
       },
     },
     href: {
@@ -100,32 +73,16 @@ export default {
   },
 } as ComponentMeta<typeof Button>;
 
-export const Default: ComponentStory<typeof Button> = (args) => (
-  <Button {...args} />
-);
-Default.args = {
-  icon: MdCelebration,
-  text: '상장하기',
-  label: '상장하기',
-};
-
 const Template: ComponentStory<typeof Button> = (args) => (
-  <Button icon={MdCelebration} text="상장하기" {...args} label="상장하기" />
+  <Button {...args}>
+    <MdCelebration />
+    상장하기
+  </Button>
 );
 
-export const AsLink: ComponentStory<typeof Button> = (args) => (
-  <Button
-    href="https://github.com/c-h-w-h/cds"
-    icon={MdCelebration}
-    text="상장하기"
-    {...args}
-    label="차가운 디자인 시스템 레포지토리 링크"
-  />
-);
-AsLink.parameters = {
-  docs: {
-    storyDescription: '링크의 역할을 하는 버튼입니다.',
-  },
+export const Default = Template.bind({});
+Default.args = {
+  label: '상장하기',
 };
 
 export const Disabled = Template.bind({});
@@ -135,58 +92,6 @@ Disabled.args = {
 Disabled.parameters = {
   docs: {
     storyDescription: 'disabled 상태에 적용되는 디자인입니다.',
-  },
-};
-
-export const IconInUrl = Template.bind({});
-IconInUrl.args = {
-  icon: 'https://user-images.githubusercontent.com/63814960/220829587-f305856c-b7bc-4dd0-a199-40735c9da5dc.png',
-  iconSize: '500px',
-};
-IconInUrl.parameters = {
-  docs: {
-    storyDescription:
-      'icon props에는 원하는 이미지 소스의 URL을 전달할 수 있습니다.',
-  },
-};
-
-export const IconOnly = Template.bind({});
-IconOnly.args = {
-  icon: MdCelebration,
-  text: undefined,
-};
-IconOnly.parameters = {
-  docs: {
-    storyDescription: 'text 없이 icon props만 사용한 버튼입니다.',
-  },
-};
-
-export const TextOnly = Template.bind({});
-TextOnly.args = {
-  icon: undefined,
-  text: '상장하기',
-};
-TextOnly.parameters = {
-  docs: {
-    storyDescription: 'icon 없이 text props만 사용한 버튼입니다.',
-  },
-};
-
-export const Width100: ComponentStory<typeof Button> = (args) => (
-  <Button
-    icon={MdCelebration}
-    text="상장하기"
-    css={css`
-      width: 100%;
-    `}
-    {...args}
-    label="상장하기"
-  />
-);
-Width100.parameters = {
-  docs: {
-    storyDescription:
-      'jsx의 css 또는 style 속성을 사용해 너비를 커스텀 할 수 있습니다.',
   },
 };
 
@@ -214,21 +119,9 @@ export const RoundLight = Template.bind({});
 RoundLight.args = {
   variant: 'round light',
 };
-
 RoundLight.parameters = {
   docs: {
     storyDescription: 'round light 버튼 디자인입니다.',
-  },
-};
-
-export const RoundIconOnly = Template.bind({});
-RoundIconOnly.args = {
-  variant: 'round',
-  text: undefined,
-};
-RoundIconOnly.parameters = {
-  docs: {
-    storyDescription: 'text 없이 icon props만 사용한 round 버튼입니다.',
   },
 };
 
@@ -240,5 +133,62 @@ Plain.parameters = {
   docs: {
     storyDescription:
       'plain 버튼입니다. background 및 border 관련 스타일링이 제외됩니다.',
+  },
+};
+
+export const IconOnly: ComponentStory<typeof Button> = () => (
+  <Flexbox justifyContent="flex-start">
+    <Button label="축하하는 버튼">
+      <MdCelebration />
+    </Button>
+    <Button variant="light" label="축하하는 버튼">
+      <MdCelebration />
+    </Button>
+    <Button variant="round" label="축하하는 버튼">
+      <MdCelebration />
+    </Button>
+    <Button variant="round light" label="축하하는 버튼">
+      <MdCelebration />
+    </Button>
+  </Flexbox>
+);
+IconOnly.parameters = {
+  docs: {
+    storyDescription: 'Icon만 사용한 버튼입니다.',
+  },
+};
+
+export const Width100: ComponentStory<typeof Button> = () => (
+  <Button
+    css={css`
+      width: 100%;
+    `}
+    label="상장하기"
+  >
+    <Icon source={MdCelebration} />
+    상장하기
+  </Button>
+);
+Width100.parameters = {
+  docs: {
+    storyDescription:
+      'jsx의 css 또는 style 속성을 사용해 너비 등의 스타일링을 커스텀 할 수 있습니다.',
+  },
+};
+
+export const AsLink: ComponentStory<typeof Button> = () => (
+  <ButtonLink
+    href="https://github.com/c-h-w-h/cds"
+    target="_blank"
+    label="차가운 디자인 시스템 레포지토리 링크"
+  >
+    <MdCelebration />
+    상장하기
+  </ButtonLink>
+);
+AsLink.parameters = {
+  docs: {
+    storyDescription:
+      '`ButtonLink` 컴포넌트입니다. HTML `<a>` 태그의 속성을 사용할 수 있습니다. 아래의 버튼 링크에는 `target="_blank"`가 설정되어 있습니다.',
   },
 };
