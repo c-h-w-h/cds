e32bb356c20726e71b0457e691b94595cc134a95
se030 <se030.kim@gmail.com>
2023-04-19 16:19:30 +0900

    feat: Drawer 기본 동작 구현

---
+++ src/components/Drawer/index.tsx
@@ -0,0 +1,110 @@
+import Portal from '@components-common/Portal';
+import { ENTER, SPACE } from '@constants/key';
+import { DRAWER_PORTAL_ROOT_ID } from '@constants/portal';
+import { css, useTheme } from '@emotion/react';
+import { ChildProps, ChildrenProps, SetState } from '@utils';
+import { KeyboardEventHandler, createContext, useState } from 'react';
+import useSafeContext from 'src/hooks/useSafeContext';
+
+import useDrawerStyle from './useDrawerStyle';
+
+export type DrawerPosition = 'bottom' | 'left';
+
+type DrawerProps = {
+  label: string;
+  position?: DrawerPosition;
+} & ChildrenProps;
+
+interface DrawerContextInterface {
+  label: string;
+  position: DrawerPosition;
+  isOpen: boolean | null;
+  setIsOpen: SetState<boolean | null>;
+}
+const DrawerContext = createContext<DrawerContextInterface | null>(null);
+
+const Drawer = ({ label, position = 'bottom', children }: DrawerProps) => {
+  const [isOpen, setIsOpen] = useState<boolean | null>(null);
+
+  const providerValue = { label, position, isOpen, setIsOpen };
+
+  return (
+    <DrawerContext.Provider value={providerValue}>
+      {children}
+    </DrawerContext.Provider>
+  );
+};
+
+const Trigger = ({ children }: ChildProps) => {
+  const { label, isOpen, setIsOpen } = useSafeContext(DrawerContext);
+
+  const onKeyDown: KeyboardEventHandler = (e) => {
+    if ([SPACE, ENTER].includes(e.key)) setIsOpen((prev) => !prev);
+  };
+
+  const triggerStyle = css`
+    width: auto;
+    height: auto;
+    cursor: pointer;
+  `;
+
+  return (
+    <div
+      css={triggerStyle}
+      aria-haspopup={true}
+      aria-expanded={!!isOpen}
+      aria-controls={`${label}-drawer`}
+      onClick={() => setIsOpen((prev) => !prev)}
+      onKeyDown={onKeyDown}
+    >
+      {children}
+    </div>
+  );
+};
+
+const Panel = ({ children }: ChildrenProps) => {
+  const { label, position, isOpen, setIsOpen } = useSafeContext(DrawerContext);
+
+  // TODO: close with Esc key
+
+  const { color } = useTheme();
+  const { black, offwhite } = color;
+
+  const { drawerStyle } = useDrawerStyle(position, offwhite);
+  const dimmerStyle = css`
+    ${isOpen ? '' : 'display: none;'}
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    overflow-y: hidden;
+    background-color: ${black};
+    opacity: 20%;
+  `;
+
+  return (
+    <Portal id={DRAWER_PORTAL_ROOT_ID}>
+      <div
+        css={dimmerStyle}
+        aria-hidden={true}
+        aria-controls={`${label}-drawer`}
+        onClick={() => setIsOpen(false)}
+      ></div>
+      <aside
+        id={`${label}-drawer`}
+        css={drawerStyle}
+        // eslint-disable-next-line no-nested-ternary
+        className={isOpen === null ? 'initial' : isOpen ? 'open' : 'close'}
+        aria-hidden={!isOpen}
+      >
+        {children}
+      </aside>
+    </Portal>
+  );
+};
+
+Drawer.Trigger = Trigger;
+Drawer.Panel = Panel;
+
+export default Drawer;
---
+++ src/components/Drawer/useDrawerStyle.tsx
@@ -0,0 +1,60 @@
+import { css, keyframes } from '@emotion/react';
+import { CSSProperties } from 'react';
+
+import type { DrawerPosition } from '.';
+
+const useDrawerStyle = (
+  position: DrawerPosition,
+  backgroundColor: CSSProperties['backgroundColor'],
+) => {
+  const slideIn = keyframes`
+      from {
+        transform: ${TRANSLATE[position]};
+      }
+  
+      to {
+        transform: translateX(0) translateY(0);
+      }
+    `;
+
+  const slideOut = keyframes`
+      from {
+        transform: translateX(0) translateY(0);
+      }
+  
+      to {
+        transform: ${TRANSLATE[position]};
+      }
+  `;
+
+  const drawerStyle = css`
+    position: absolute;
+    ${STYLE[position]}
+    transform: ${TRANSLATE[position]};
+    background-color: ${backgroundColor};
+
+    &.open {
+      animation: ${slideIn} 0.5s;
+      transform: translateX(0) translateY(0);
+    }
+
+    &.close {
+      animation: ${slideOut} 0.5s;
+      transform: ${TRANSLATE[position]};
+    }
+  `;
+
+  return { drawerStyle };
+};
+
+export default useDrawerStyle;
+
+const STYLE: Record<DrawerPosition, string> = {
+  bottom: 'bottom: 0; width: 100%;',
+  left: 'top: 0; left: 0; height: 100%;',
+};
+
+const TRANSLATE: Record<DrawerPosition, CSSProperties['transform']> = {
+  bottom: 'translateY(100%)',
+  left: 'translateX(-100%)',
+};
