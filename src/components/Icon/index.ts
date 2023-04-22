a6aa5879148cf304f8bdf06cbf7f2ea2a3ffd87d
se030 <se030.kim@gmail.com>
2023-04-22 18:39:11 +0900

    feat: CDS 아이콘 추가
    - 공통 props로 size, color 사용
    - 사용처의 loader를 알 수 없어 하드코딩된 인라인 svg로 제공

---
+++ src/components/Icon/index.ts
@@ -0,0 +1,6 @@
+export { default as CdsBag } from './CdsBag';
+export { default as CdsHamburger } from './CdsHamburger';
+export { default as CdsHeart } from './CdsHeart';
+export { default as CdsHome } from './CdsHome';
+export { default as CdsSearch } from './CdsSearch';
+export { default as CdsUser } from './CdsUser';
