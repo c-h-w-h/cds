25ae349a34f96cfc1108a50bcdcee3a60781649c
sunmin <81913106+leesunmin1231@users.noreply.github.com>
2023-04-24 16:26:30 +0900

    Feat/input: Input 컴포넌트 리팩토링 (#103)
    * feat: input 컴포넌트 구현
    
    * feat: input 제어 컴포넌트 구현
    
    * feat: focus 효과, validate 스타일 적용
    
    * refactor: props 수정
    
    * feat: id, name attribute 추가
    
    * feat: storybook id 추가
    
    * fix: input 태그 self closing
    
    * fix: forwordRef 로 props 변경
    
    * fix: css props 변경
    
    * refactor: 중복 코드 제거
    
    * refactor: input 컴포넌트 icon 추가
    
    * refactor: icon test용 props 제거
    
    * fix: cancel 버튼 오류 수정
    
    * fix: setIsvalidate useEffect 로 변경
    
    * fix: ref props forwardRef로 수정
    
    * fix: input 태그 분기 제거
    
    * fix: 필요 없는 props 타입 제거
    
    * fix: forward ref로 변경
    
    * fix: props setInputValue로 변경
    
    * fix: 한글 입력 깨짐 수정
    
    * fix: unControlled component 리렌더링 방지
    
    * refactor: validate 기준 변경
    
    * refactor: 제어컴포넌트 체크 상수 추가
    
    * feat: focus 스타일
    
    * refactor: props onChange, onCancel로 변경
    
    * fix: isControlled props 추가
    
    * feat: isControlled props 삭제
    
    * feat: label element 추가
    
    * fix: padding 값 수정
    
    * fix: input element isControlled 분기처리 변경
    
    * fix: 외부 상태 변경시 내부 상태도 변경
    
    * refactor: label props 제거
    
    * refactor: cancel, leading icon props type 변경
    
    * refactor: 필요없는 상태 제거
    
    * feat: padding 값 수정
    
    * fix: storybook 디렉토리 변경
    
    * refactor: 스토리북 버튼 사이즈 수정

--- src/components/Input/Input.stories.tsx
+++ src/components/Input/Input.stories.tsx
@@ -1,96 +1,122 @@
-import { css } from '@emotion/react';
+import Button from '@components/Button';
+import styled from '@emotion/styled';
 import { useRef, useState } from '@storybook/addons';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { useEffect } from 'react';
+import { MdSearch } from 'react-icons/md';
 
 import Input from '.';
 
 export default {
-  title: 'Input',
+  title: 'Components/Input',
   component: Input,
   parameters: {
     layout: 'fullscreen',
+    componentSubtitle:
+      'Input은 사용자의 입력을 받는 컴포넌트입니다. 제어, 비제어 두 방식 모두 지원합니다.',
+  },
+  argTypes: {
+    placeholder: {
+      name: 'placeholder',
+      description: 'Input 내부 placeholder를 설정합니다.',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: '입력하세요' },
+      },
+    },
+    id: {
+      name: 'id',
+      description: 'Input 컴포넌트 id attribute를 설정합니다.',
+      table: {
+        type: { summary: 'string' },
+      },
+    },
+    name: {
+      name: 'name',
+      description: 'Input 컴포넌트 name attribute를 설정합니다.',
+      table: {
+        type: { summary: 'string' },
+      },
+    },
+    type: {
+      name: 'type',
+      description: 'Input 컴포넌트 type attribute를 설정합니다.',
+      table: {
+        type: { summary: 'string' },
+      },
+    },
+    onChange: {
+      name: 'onChange',
+      description: 'Input 컴포넌트 change 이벤트 Handler 입니다.',
+    },
+
+    onCancel: {
+      name: 'onCancel',
+      description: 'Input 컴포넌트 내에 취소버튼 click 이벤트 Handler 입니다.',
+    },
+    defaultValue: {
+      name: 'defaultValue',
+      description: '비제어 Input 컴포넌트 사용시 초기값을 설정합니다.',
+      table: {
+        type: { summary: 'string' },
+      },
+    },
+
+    value: {
+      name: 'value',
+      description: '제어 Input 컴포넌트 사용시 초기값을 설정합니다.',
+      table: {
+        type: { summary: 'string' },
+      },
+    },
+
+    isValid: {
+      name: 'isValid',
+      description:
+        '제어 Input 컴포넌트 사용시 validation 여부를 체크하여 스타일에 반영합니다.',
+      table: {
+        type: { summary: 'boolean' },
+        defaultValue: { summary: 'true' },
+      },
+    },
+
+    leadingIcon: {
+      name: 'leadingIcon',
+      description: 'Input 컴포넌트 앞에 오는 아이콘을 설정합니다.',
+      table: {
+        type: { summary: 'IconSource' },
+      },
+    },
+
+    isClearable: {
+      name: 'isClearable',
+      description: 'Input 컴포넌트 뒤에 오는 취소 아이콘 유무를 설정합니다.',
+      table: {
+        type: { summary: 'boolean' },
+      },
+    },
   },
 } as ComponentMeta<typeof Input>;
 
-export const UncontrolledDefaultStyle: ComponentStory<typeof Input> = (
-  args,
-) => {
-  const inputRef = useRef<HTMLInputElement | null>(null);
-  const [inputValue, setInputValue] = useState<string>('');
-  return (
-    <>
-      <Input {...args} forwordRef={inputRef} />
-      <button
-        type="submit"
-        onClick={() =>
-          setInputValue(inputRef.current ? inputRef.current.value : '')
-        }
-      >
-        제출
-      </button>
-      <div>{inputValue}</div>
-    </>
-  );
-};
-
-UncontrolledDefaultStyle.args = {
-  placeholder: '입력하세요',
-  id: 'uncontrolled',
-};
-
-export const UncontrolledCustomStyle: ComponentStory<typeof Input> = (args) => {
-  const inputRef = useRef<HTMLInputElement | null>(null);
-  const [inputValue, setInputValue] = useState<string>('');
-  return (
-    <>
-      <Input
-        {...args}
-        forwordRef={inputRef}
-        css={css`
-          width: 300px;
-          height: 34px;
-          &::placeholder {
-            color: violet;
-            font-style: italic;
-          }
-        `}
-      />
-      <button
-        type="submit"
-        onClick={() =>
-          setInputValue(inputRef.current ? inputRef.current.value : '')
-        }
-      >
-        제출
-      </button>
-      <div>{inputValue}</div>
-    </>
-  );
-};
-UncontrolledCustomStyle.args = {
-  placeholder: '입력하세요',
-  id: 'uncontrolled',
-};
-
-export const Controlled: ComponentStory<typeof Input> = (args) => {
+const ControlledWithCancelTemplate: ComponentStory<typeof Input> = (args) => {
   const [inputValue, setInputValue] = useState<string>('');
   const [isValidate, setIsValidate] = useState<boolean>(true);
-  const validateHandler = (target: EventTarget & HTMLInputElement) => {
-    if (target.value.length < 5) {
+
+  useEffect(() => {
+    if (inputValue.length < 5 && inputValue.length > 0) {
       setIsValidate(false);
     } else {
       setIsValidate(true);
     }
-  };
-  const changeHandler = (target: EventTarget & HTMLInputElement) => {
-    validateHandler(target);
-    setInputValue(target.value);
-  };
+  }, [inputValue]);
+
   return (
     <>
       <Input
         {...args}
-        onChange={({ target }) => changeHandler(target)}
+        onChange={({ target }) => setInputValue(target.value)}
+        onCancel={() => setInputValue('')}
+        value={inputValue}
         isValid={isValidate}
       />
       {!isValidate && (
@@ -103,7 +129,127 @@ export const Controlled: ComponentStory<typeof Input> = (args) => {
   );
 };
 
+export const Default = ControlledWithCancelTemplate.bind({});
+Default.args = {
+  placeholder: '입력하세요',
+  id: 'controlled',
+  leadingIcon: <MdSearch />,
+  isClearable: true,
+};
+Default.parameters = {
+  docs: {
+    storyDescription: '취소 버튼을 설정한 제어 Input 컴포넌트입니다.',
+  },
+};
+
+const ControlledTemplate: ComponentStory<typeof Input> = (args) => {
+  const [inputValue, setInputValue] = useState<string>('');
+  const [isValidate, setIsValidate] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (inputValue.length < 5 && inputValue.length > 0) {
+      setIsValidate(false);
+    } else {
+      setIsValidate(true);
+    }
+  }, [inputValue]);
+
+  return (
+    <>
+      <Input
+        {...args}
+        onChange={({ target }) => setInputValue(target.value)}
+        value={inputValue}
+        isValid={isValidate}
+      />
+      {!isValidate && (
+        <div style={{ color: 'red', fontSize: '10px' }}>
+          5글자 이상 입력하세요.
+        </div>
+      )}
+      <div>{inputValue}</div>
+    </>
+  );
+};
+export const Controlled = ControlledTemplate.bind({});
 Controlled.args = {
   placeholder: '입력하세요',
   id: 'controlled',
 };
+
+Controlled.parameters = {
+  docs: {
+    storyDescription: '기본 제어 Input 컴포넌트입니다.',
+  },
+};
+
+export const ControlledWithCancel = ControlledWithCancelTemplate.bind({});
+ControlledWithCancel.args = {
+  placeholder: '입력하세요',
+  id: 'controlled',
+  leadingIcon: <MdSearch />,
+  isClearable: true,
+};
+ControlledWithCancel.parameters = {
+  docs: {
+    storyDescription: '취소 버튼을 설정한 제어 Input 컴포넌트입니다.',
+  },
+};
+
+const UncontrolledTemplate: ComponentStory<typeof Input> = (args) => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const [inputValue, setInputValue] = useState<string>('');
+  return (
+    <>
+      <UncontrolledWrapper>
+        <Input {...args} ref={inputRef} />
+        <Button
+          onClick={() =>
+            setInputValue(inputRef.current ? inputRef.current.value : '')
+          }
+          text="제출"
+        />
+      </UncontrolledWrapper>
+      <div>{inputValue}</div>
+    </>
+  );
+};
+
+export const Uncontrolled = UncontrolledTemplate.bind({});
+Uncontrolled.args = {
+  placeholder: '입력하세요',
+  id: 'uncontrolled',
+};
+Uncontrolled.parameters = {
+  docs: {
+    storyDescription: '기본 비제어 Input 컴포넌트입니다.',
+  },
+};
+
+export const UncontrolledWithDefaultValue = UncontrolledTemplate.bind({});
+UncontrolledWithDefaultValue.args = {
+  placeholder: '입력하세요',
+  id: 'uncontrolled',
+  defaultValue: '초기값',
+};
+UncontrolledWithDefaultValue.parameters = {
+  docs: {
+    storyDescription: '초기값을 설정해준 비제어 Input 컴포넌트입니다.',
+  },
+};
+
+export const UncontrolledWithLeadingIcon = UncontrolledTemplate.bind({});
+UncontrolledWithLeadingIcon.args = {
+  placeholder: '입력하세요',
+  id: 'uncontrolled',
+  leadingIcon: <MdSearch />,
+};
+UncontrolledWithDefaultValue.parameters = {
+  docs: {
+    storyDescription: 'leadingIcon을 설정해준 비제어 Input 컴포넌트입니다.',
+  },
+};
+
+const UncontrolledWrapper = styled.div`
+  display: flex;
+`;
