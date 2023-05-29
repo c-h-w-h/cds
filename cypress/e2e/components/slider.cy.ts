10fef6d3f552a7dc5751681c780488f8ed2c9863
iyu88 <iyu88@naver.com>
2023-05-30 02:53:05 +0900

    test: Page Up, Down 키보드 이벤트 추가

--- cypress/e2e/components/slider.cy.ts
+++ cypress/e2e/components/slider.cy.ts
@@ -63,5 +63,15 @@ describe('Slider component test', () => {
       cy.get("@thumb").trigger('keydown', { keyCode : 40 });
       cy.get('@thumb').invoke('text').should('eq', '49');
     });
+
+    it('Increase with page up', () => {
+      cy.get("@thumb").trigger('keydown', { keyCode : 33 });
+      cy.get('@thumb').invoke('text').should('eq', '60');
+    });
+
+    it('Decrease with page down', () => {
+      cy.get("@thumb").trigger('keydown', { keyCode : 34 });
+      cy.get('@thumb').invoke('text').should('eq', '40');
+    });
   });
 });
