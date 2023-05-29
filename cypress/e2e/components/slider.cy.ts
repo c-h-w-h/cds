91fa0ceeaddeb2906d2498acd0cddfc6ddc40de8
iyu88 <iyu88@naver.com>
2023-05-29 17:02:54 +0900

    feat: alias 초기화 함수 선언

--- cypress/e2e/components/slider.cy.ts
+++ cypress/e2e/components/slider.cy.ts
@@ -4,15 +4,22 @@ const FILLED_ID = `${SLIDER_PREFIX}-filled`;
 const THUMB_ID = `${SLIDER_PREFIX}-thumb`;
 
 describe('Slider component test', () => {
-  beforeEach(() => {
-    cy.visitStory('components-slider');
-    cy.getStory('slider', 'default').as('slider');
+  const initAlias = (storyName: string) => {
+    cy.getStory('slider', storyName).as('slider');
     cy.get('@slider').find(TRACK_ID).as('track');
     cy.get('@slider').find(FILLED_ID).as('filled');
     cy.get('@slider').find(THUMB_ID).as('thumb');
+  }
+
+  beforeEach(() => {
+    cy.visitStory('components-slider');
   });
 
   describe('Default slider', () => {
+    beforeEach(() => {
+      initAlias('default');
+    })
+
     it('Click min value', () => {
       cy.get('@track').click('left');
       cy.get('@thumb').invoke('text').should('eq', '0');  
